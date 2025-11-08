9a75d41ba860763df41c166a7483c57bca2b1d71
inogai <inogai.sign@gmail.com>
2025-11-08 19:00:06 +0800

    keys: flash

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -1,3 +1,13 @@
 U.packadd "flash.nvim"
 
-require("flash").setup()
+local flash = require "flash"
+
+flash.setup {}
+
+require "which-key".add {
+  { "s",     mode = { "n", "x", "o" }, flash.jump,              desc = "Flash" },
+  { "S",     mode = { "n", "x", "o" }, flash.treesitter,        desc = "Flash Treesitter" },
+  { "r",     mode = "o",               flash.remote,            desc = "Remote Flash" },
+  { "R",     mode = { "o", "x" },      flash.treesitter_search, desc = "Treesitter Search" },
+  { "<C-s>", mode = { "c" },           flash.toggle,            desc = "Toggle Flash Search" },
+}
