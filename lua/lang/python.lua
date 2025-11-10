c537d66decc0365db7c45ae384d2ae04fc5dafd0
inogai <inogai.sign@gmail.com>
2025-11-10 22:28:35 +0800

    fix: venv-selector should not be lazy-loaded on ft to ensure venv is recovered properly

--- lua/lang/python.lua
+++ lua/lang/python.lua
@@ -3,17 +3,14 @@ vim.lsp.enable({
   'ruff',
 })
 
-U.autocmd('FileType', {
-  pattern = { 'python' },
-  callback = function()
-    U.packadd('venv-selector.nvim')
-    require('venv-selector').setup({})
+U.on('User', 'VeryLazy', function()
+  U.packadd('venv-selector.nvim')
+  require('venv-selector').setup({})
 
-    vim.keymap.set(
-      'n',
-      '<localleader>v',
-      '<cmd>VenvSelect<cr>',
-      { desc = 'Select virtual environment' }
-    )
-  end,
-})
+  vim.keymap.set(
+    'n',
+    '<localleader>v',
+    '<cmd>VenvSelect<cr>',
+    { desc = 'Select virtual environment' }
+  )
+end)
