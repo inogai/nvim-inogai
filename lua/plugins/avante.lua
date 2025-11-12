42e10dde8581a3265883db00b2df504bd79c7ebd
inogai <inogai.sign@gmail.com>
2025-11-12 12:15:36 +0800

    ref(avante): add moonshoot api and <C-a> bind

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -1,25 +1,33 @@
-U.autocmd('User', {
-  pattern = 'VeryLazy',
-  callback = function()
-    U.packadd('blink-cmp-avante')
-    U.packadd('avante.nvim')
+U.packadd('blink-cmp-avante')
+U.packadd('avante.nvim')
 
-    require('avante').setup({
-      instructions_file = 'AGENTS.md',
-      provider = 'copilot',
-      providers = {
-        copilot = {
-          model = 'grok-code-fast-1', -- claude-sonnet-4.5
-        },
+require('avante').setup({
+  instructions_file = 'AGENTS.md',
+  provider = 'copilot',
+  providers = {
+    copilot = {
+      model = 'grok-code-fast-1', -- claude-sonnet-4.5
+    },
+    moonshot = {
+      endpoint = 'https://api.moonshot.ai/v1',
+      model = 'kimi-k2-0905-preview',
+      timeout = 30000,
+      extra_request_body = {
+        temperature = 0.75,
+        max_tokens = 32768,
       },
-    })
+    },
+  },
+})
 
-    U.blink_add_source('avante', {
-      module = 'blink-cmp-avante',
-      name = 'Avante',
-      opts = {
-        -- options for blink-cmp-avante
-      },
-    })
-  end,
+local api = require('avante.api')
+
+require('which-key').add({
+  { '<C-a>', api.ask, desc = 'Ask AI', mode = 'nv' },
+})
+
+U.blink_add_source('avante', {
+  module = 'blink-cmp-avante',
+  name = 'Avante',
+  opts = {},
 })
