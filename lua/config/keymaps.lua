bf5621cdfd35b32aa49b313936141a3466888908
inogai <inogai.sign@gmail.com>
2025-11-12 22:17:04 +0800

    ref(fzf-lua): move keys

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,18 +1,13 @@
 local wk = require('which-key')
 
-local function F() return require('fzf-lua') end
-
 -- stylua: ignore
 wk.add({
-  { '<D-s>',           '<cmd>w<cr><esc>',                                          desc = '[S]ave File',           mode = 'nixs' },
-  { '<F2>',            vim.lsp.buf.rename,                                         desc = 'LSP Rename',            mode = 'nixs' },
-  { '<esc>',           '<cmd>nohlsearch<cr><esc>',                                 desc = 'Clear Search Highlight' },
+  { '<D-s>', '<cmd>w<cr><esc>',                             desc = '[S]ave File',           mode = 'nixs' },
+  { '<F2>',  vim.lsp.buf.rename,                            desc = 'LSP Rename',            mode = 'nixs' },
+  { '<esc>', '<cmd>nohlsearch<cr><esc>',                    desc = 'Clear Search Highlight' },
 
-  { '<leader><space>', '<cmd>FzfLua files<cr>',                                    desc = 'Find Files (Root Dir)' },
-  { '<leader>,',       '<cmd>FzfLua buffers sort_mru=true sort_lastused=true<cr>', desc = 'Switch Buffer', },
-
-  { 'zR',              function() require('ufo').openAllFolds() end,               desc = 'Open all folds' },
-  { 'zM',              function() require('ufo').closeAllFolds() end,              desc = 'Close all folds' },
+  { 'zR',    function() require('ufo').openAllFolds() end,  desc = 'Open all folds' },
+  { 'zM',    function() require('ufo').closeAllFolds() end, desc = 'Close all folds' },
 
   {
     'K',
@@ -46,55 +41,21 @@ wk.add({
     desc = 'Prev Trouble',
   },
 
-  { '<leader>e', '<cmd>Yazi<cr>' },
-  { '<leader>E', '<cmd>Yazi cwd<cr>' },
+  { '<leader>e',  '<cmd>Yazi<cr>' },
+  { '<leader>E',  '<cmd>Yazi cwd<cr>' },
   -- goto
-  { 'g',         desc = '+[G]oto' },
-  { 'ga',        desc = '+[A]dd Surrounding' },
-  { 'gs',        desc = '+[S]ubtract Surrounding' },
-  { 'ge',        desc = '+[E]dit Surrounding' },
-  { 'gd',        function() F().lsp_definitions({ jump1 = true, jump_on_cb = true }) end, desc = '[D]efinitions' },
-  { 'gr',        '<cmd>FzfLua lsp_references<cr>',                                        desc = '[R]erferences' },
-  {
-    'gy',
-    '<cmd>FzfLua lsp_typedefs<cr>',
-    desc = 'T[y]pe Definition',
-  },
-  {
-    '<C-.>',
-    '<cmd>FzfLua lsp_code_actions<cr>',
-    desc = 'Code Actions',
-    mode = 'nxis',
-  },
+  { 'g',          desc = '+[G]oto' },
+  { 'ga',         desc = '+[A]dd Surrounding' },
+  { 'gs',         desc = '+[S]ubtract Surrounding' },
+  { 'ge',         desc = '+[E]dit Surrounding' },
 
   { '<leader>s',  desc = '+Search' },
-  { '<leader>s"', '<cmd>FzfLua registers<cr>',                 desc = 'Registers' },
-  { '<leader>sa', '<cmd>FzfLua autocmds<cr>',                  desc = 'Auto Commands' },
-  { '<leader>sb', '<cmd>FzfLua grep_curbuf<cr>',               desc = 'Buffer' },
-  { '<leader>sc', '<cmd>FzfLua command_history<cr>',           desc = 'Command History' },
-  { '<leader>sC', '<cmd>FzfLua commands<cr>',                  desc = 'Commands' },
-  { '<leader>sd', '<cmd>FzfLua diagnostics_document<cr>',      desc = 'Document Diagnostics' },
-  { '<leader>sD', '<cmd>FzfLua diagnostics_workspace<cr>',     desc = 'Workspace Diagnostics' },
-  { '<leader>sg', '<cmd>FzfLua live_grep<cr>',                 desc = 'Grep' },
-  { '<leader>sh', function() F().helptags() end,               desc = '[H]elp' },
-  { '<leader>sH', '<cmd>FzfLua highlights<cr>',                desc = 'Search Highlight Groups' },
-  { '<leader>sk', '<cmd>FzfLua keymaps<cr>',                   desc = 'Key Maps' },
-  { '<leader>sm', '<cmd>FzfLua marks<cr>',                     desc = 'Jump to Mark' },
   { '<leader>sn', function() require('noice').cmd('pick') end, desc = '[N]oice' },
   { '<leader>sr', '<cmd>GrugFar<cr>',                          desc = '[R]eplace' },
-  { '<leader>sR', '<cmd>FzfLua resume<cr>',                    desc = 'Resume' },
-  { '<leader>sq', '<cmd>FzfLua quickfix<cr>',                  desc = 'Quickfix List' },
-  { '<leader>ss', function() F().lsp_document_symbols() end,   desc = 'Goto Symbol' },
-  {
-    '<leader>sS',
-    function() F().lsp_live_workspace_symbols() end,
-    desc = 'Goto Symbol (Workspace)',
-  },
 
   { '<leader>g',  desc = '+[G]it' },
-  { '<leader>gl', function() Snacks.lazygit.open() end, desc = '[L]azygit' },
+  { '<leader>gl', function() Snacks.lazygit.open() end,        desc = '[L]azygit' },
 
-  { '<leader>ua', '<cmd>Copilot toggle<cr>',            desc = '[A]I Completions' },
-  { '<leader>uC', '<cmd>FzfLua colorschemes<cr>',       desc = '[C]olorscheme' },
-  { '<leader>ui', '<cmd>Inspect<cr>',                   desc = '[I]nspect' },
+  { '<leader>ua', '<cmd>Copilot toggle<cr>',                   desc = '[A]I Completions' },
+  { '<leader>ui', '<cmd>Inspect<cr>',                          desc = '[I]nspect' },
 })
--- lua/lze_specs/fzf-lua.lua
+++ lua/lze_specs/fzf-lua.lua
@@ -3,11 +3,11 @@ return {
   event = 'VimEnter',
   after = function()
     local actions = require('fzf-lua.actions')
-    local config = require('fzf-lua.config')
+    local F = require('fzf-lua')
 
-    require('fzf-lua').setup({
-      -- config.defaults.actions.files['ctrl-t'] = require('trouble.sources.fzf').open
+    F.register_ui_select()
 
+    F.setup({
       fzf_colors = true,
       defaults = {
         formatter = 'path.filename_first', -- formatter = 'path.dirname_first',
@@ -32,6 +32,32 @@ return {
       },
     })
 
-    require('fzf-lua').register_ui_select()
+    -- stylua: ignore
+    require('which-key').add({
+      { '<leader> ',  '<cmd>FzfLua files<cr>',                                               desc = 'Find Files (Root Dir)', },
+      { '<leader>,',  '<cmd>FzfLua buffers sort_mru=true sort_lastused=true<cr>',            desc = 'Switch Buffer', },
+      { 'gd',         function() F.lsp_definitions({ jump1 = true, jump_on_cb = true }) end, desc = '[D]efinitions', },
+      { 'gr',         '<cmd>FzfLua lsp_references<cr>',                                      desc = '[R]erferences', },
+      { 'gy',         '<cmd>FzfLua lsp_typedefs<cr>',                                        desc = 'T[y]pe Definition', },
+      { '<C-.>',      '<cmd>FzfLua lsp_code_actions<cr>',                                    desc = 'Code Actions',            mode = 'nxis', },
+      { '<leader>s"', '<cmd>FzfLua registers<cr>',                                           desc = 'Registers' },
+      { '<leader>sa', '<cmd>FzfLua autocmds<cr>',                                            desc = 'Auto Commands', },
+      { '<leader>sb', '<cmd>FzfLua grep_curbuf<cr>',                                         desc = 'Buffer' },
+      { '<leader>sc', '<cmd>FzfLua command_history<cr>',                                     desc = 'Command History', },
+      { '<leader>sC', '<cmd>FzfLua commands<cr>',                                            desc = 'Commands' },
+      { '<leader>sd', '<cmd>FzfLua diagnostics_document<cr>',                                desc = 'Document Diagnostics', },
+      { '<leader>sD', '<cmd>FzfLua diagnostics_workspace<cr>',                               desc = 'Workspace Diagnostics', },
+      { '<leader>sg', '<cmd>FzfLua live_grep<cr>',                                           desc = 'Grep' },
+      { '<leader>sh', function() F.helptags() end,                                           desc = '[H]elp', },
+      { '<leader>sH', '<cmd>FzfLua highlights<cr>',                                          desc = 'Search Highlight Groups', },
+      { '<leader>sk', '<cmd>FzfLua keymaps<cr>',                                             desc = 'Key Maps' },
+      { '<leader>sm', '<cmd>FzfLua marks<cr>',                                               desc = 'Jump to Mark', },
+      { '<leader>sR', '<cmd>FzfLua resume<cr>',                                              desc = 'Resume' },
+      { '<leader>sq', '<cmd>FzfLua quickfix<cr>',                                            desc = 'Quickfix List', },
+      { '<leader>ss', function() F.lsp_document_symbols() end,                               desc = 'Goto Symbol', },
+      { '<leader>sS', function() F.lsp_live_workspace_symbols() end,                         desc = 'Goto Symbol (Workspace)', },
+
+      { '<leader>uC', '<cmd>FzfLua colorschemes<cr>',                                        desc = '[C]olorscheme' },
+    })
   end,
 }
