9987007be360117b463242bcf2454e4324ac4d48
inogai <inogai.sign@gmail.com>
2025-11-13 18:20:10 +0800

    fix: ufo

--- lua/lze_specs/ufo.lua
+++ lua/lze_specs/ufo.lua
@@ -1,18 +1,16 @@
 return {
-  {
-    'nvim-ufo',
-    event = 'VimEnter',
-    config = function()
-      require('ufo').setup({
-        provider_selector = function(_bufnr, filetype, _buftype)
-          local main = 'lsp'
-          local fallback = 'indent'
+  'nvim-ufo',
+  event = 'VimEnter',
+  after = function()
+    require('ufo').setup({
+      provider_selector = function(_bufnr, filetype, _buftype)
+        local main = 'lsp'
+        local fallback = 'indent'
 
-          if filetype == 'markdown' then main = 'treesitter' end
+        if filetype == 'markdown' then main = 'treesitter' end
 
-          return { main, fallback }
-        end,
-      })
-    end,
-  },
+        return { main, fallback }
+      end,
+    })
+  end,
 }
