89a79cf8b0357d67b70a1a99887a1c2123d0e26a
inogai <inogai.sign@gmail.com>
2025-11-28 22:43:35 +0800

    fix: remove unsensible import_dir call

--- lua/config/neovim.lua
+++ lua/config/neovim.lua
@@ -1,11 +1,5 @@
 vim.cmd.colorscheme('moegi')
 
--- Load language configurations immediately
-U.import_dir('lang')
-
--- Plugin configurations are now handled by lze automatically
--- The lze_specs directory is loaded by lze_config.setup()
-
 return {
   { import = 'lze_specs' },
   { import = 'lang' },
--- lua/config/vscode.lua
+++ lua/config/vscode.lua
@@ -1,7 +1,11 @@
+--- VSCode Neovim Configuration
+---
+--- Selective enable plugins for VSCode Neovim environment
+
 return {
   { import = 'lze_specs._treesitter' },
   { import = 'lze_specs._lspconfig' },
   { import = 'lze_specs._which-key' },
   { import = 'lze_specs.flash' },
-  { import = 'lze_specs.mini' },
+  { import = 'lze_specs.mini' }, -- NOTE: may want to only enable certain mini.* plugins in future
 }
