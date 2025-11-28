ba9380ea769e87933d17b8263f237927ee0dd2da
inogai <inogai.sign@gmail.com>
2025-11-28 22:34:46 +0800

    ref(init): add lazy default options

--- init.lua
+++ init.lua
@@ -4,8 +4,12 @@ require('config.options')
 
 if vim.env['NVIM_DEV_MODULE'] then require('config.dev') end
 
+---@type lze.Config
 vim.g.lze = {
   load = U.packadd,
+  verbose = true,
+  default_priority = 50,
+  without_default_handlers = false,
 }
 
 require('lze').load({
