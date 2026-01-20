bf332e404a95c57ebc1aa275255e53ef35695c1a
inogai <inogai.sign@gmail.com>
2026-01-20 18:04:47 +0800

    feat(obsidian.nvim): add dynamic workspace discovery

--- lua/lze_specs/obsidian-nvim.lua
+++ lua/lze_specs/obsidian-nvim.lua
@@ -1,11 +1,27 @@
-local workspace_path = vim.fn.expand('~/Documents/Obsidian/v2')
+local WORKSPACE_ROOT = vim.fn.expand('~/Documents/Obsidian')
+
+local function find_workspaces(root)
+  local ret = {}
+  local dirs = vim.fn.globpath(root, '*', false, true)
+
+  for _, dir in ipairs(dirs) do
+    if vim.fn.isdirectory(dir) == 1 then
+      table.insert(ret, {
+        name = vim.fn.fnamemodify(dir, ':t'),
+        path = dir,
+      })
+    end
+  end
+
+  return ret
+end
 
 return {
   {
     'obsidian.nvim',
     events = {
-      { 'BufReadPre', pattern = workspace_path .. '/**' },
-      { 'BufNewFile', pattern = workspace_path .. '/**' },
+      { 'BufReadPre', pattern = WORKSPACE_ROOT .. '/**' },
+      { 'BufNewFile', pattern = WORKSPACE_ROOT .. '/**' },
     },
     after = function()
       print('Loading Obsidian.nvim...')
@@ -18,12 +34,7 @@ return {
         },
         legacy_commands = false,
         ui = { enable = false }, -- in flavour of render-markdown.nvim
-        workspaces = {
-          {
-            name = 'v2',
-            path = workspace_path,
-          },
-        },
+        workspaces = find_workspaces(WORKSPACE_ROOT),
       })
     end,
     keys = {
